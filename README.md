b054cb9236a3c62aa6e90e29a3bd90a5a02626cc
mfrankmysql <mike.frank@oracle.com>
2026-06-25 12:31:40 -0500

    Update MySQL Shell Audit Tool plugin files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# mysqlshellplugs
-These are additional tools for managing MySQL
+# MySQL Audit Tool Plugin
+
+Independent MySQL Shell plugin for creating and assigning MySQL Enterprise Audit filters through an interactive wizard.
+
+## Status
+
+This project is an independent community tool. It is not part of MySQL, MySQL Shell, Oracle, or any official MySQL source branch, and it is not endorsed or supported by Oracle.
+
+## License and warranty
+
+This project is distributed under the MIT License and is provided on an "AS IS" basis, without warranties or conditions of any kind. See `LICENSE` for the full license text.
+
+## Requirements
+
+- MySQL Shell with Python plugin support
+- MySQL Enterprise Audit plugin or audit log component installed on the target server
+- Sufficient privileges to manage audit filters and audit user assignments
--- wizard.py
+++ wizard.py
@@ -1,25 +1,16 @@
-# Copyright (c) 2020, Oracle and/or its affiliates.
+# Copyright (c) 2026 Mike
+# SPDX-License-Identifier: MIT
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License, version 2.0,
-# as published by the Free Software Foundation.
+# This is an independent community MySQL Shell plugin. It is not part of
+# MySQL, MySQL Shell, Oracle, or any official MySQL source tree.
 #
-# This program is also distributed with certain software (including
-# but not limited to OpenSSL) that is licensed under separate terms, as
-# designated in a particular file or component or in included license
-# documentation.  The authors of MySQL hereby grant you an additional
-# permission to link the program and your derivative works with the
-# separately licensed software that they have included with MySQL.
-# This program is distributed in the hope that it will be useful,  but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
-# the GNU General Public License, version 2.0, for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+# Distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the LICENSE file for details.
 
-"""Sub-Module for supporting DISA STIG Checks"""
+"""MySQL Enterprise Audit filter wizard for MySQL Shell.
+
+This module registers audittool.start.wizard().
+"""
 
 
 from mysqlsh.plugin_manager import plugin_function
